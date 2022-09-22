b66a20c60ff20c31ac408ef2b901148da86aa9bc
raadkasem <rkasem0@gmail.com>
2022-09-22 15:37:16 +0300

    Registering a new RepositoriesServiceProvider and bind an instance of CustomerRepoInterface to the app

--- laravel/config/app.php
+++ laravel/config/app.php
@@ -175,6 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\ConfigServiceProvider::class,
+        App\Providers\RepositoriesServiceProvider::class,
 
 
     ],
