b11e97f18428a6cc9fc60e2ca3008490da1b7246
raadkasem <rkasem0@gmail.com>
2022-09-22 13:25:53 +0300

    retrieve all customers

--- laravel/app/Http/Controllers/CustomerController.php
+++ laravel/app/Http/Controllers/CustomerController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
-    //
+    public function index()
+    {
+        return Customer::all();
+    }
 }
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -17,5 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
 //    Debugbar::info('hey');
     return view('welcome');
-    dd(public_path('public'));
 });
+
+Route::get('/customers', 'App\Http\Controllers\CustomerController@index');
